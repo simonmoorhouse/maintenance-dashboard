84db40466e2b4d6b491681ab972983858f8adf44
simonmoorhouse <simon.moorhouse@dalkia.co.uk>
2025-05-22 14:35:19 +0100

    Delete components directory

--- components/ui/card.js
+++
@@ -1,6 +0,0 @@
-export function Card({ children, className }) {
-  return <div className={`rounded-2xl shadow p-2 bg-white ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>;
-}
--- components/ui/input.js
+++
@@ -1,3 +0,0 @@
-export function Input({ ...props }) {
-  return <input className="w-full p-2 border rounded" {...props} />;
-}
